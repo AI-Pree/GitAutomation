232b55756296dc32eee8ebb7b500dc49ea667137
preeyashp <preeyashp@gmail.com>
2020-01-31 18:14:15 +1100

    'Initial commit'

---
+++ requestHandler.py
@@ -0,0 +1,65 @@
+import requests
+import logging
+import json
+import subprocess
+import os
+from contextlib import contextmanager
+
+class requestHandler():
+    def __init__(self, **config):
+        self.__dict__.update(**config)
+        self.git_api = "https://api.github.com"
+        self.response = requests.get(self.git_api)
+        self.logger = logging.getLogger()
+        self.session = requests.Session()
+
+    def connect(self):
+        if hasattr(self,"api_token"):
+            self.session.headers['Authorization'] = "token {key}".format(key = self.api_token)
+        connect = self.session.post(self.git_api)
+        self.logger.info("authenticated successfully") if connect == 200 else self.logger.error(self.session.get(self.git_api).status_code)
+    
+    @contextmanager
+    def cd(self,newdir):
+        prevDir = os.getcwd()
+        os.chdir(os.path.expanduser(newdir))
+        try:
+            yield
+        finally:
+            os.chdir(prevDir)
+
+
+    #push the whole project in the dir to the github
+    def push_project_github(self,username, reponame, path):
+        with self.cd(path):
+            subprocess.run(["git", "remote", "add", "origin", "https://github.com/" + username + "/" + reponame + ".git"])
+            subprocess.run(["git", "add", "."])
+            subprocess.run(["git", "commit", "-m", "'Initial commit'"])
+            subprocess.run(["git", "push", "-u", "origin", "master"])
+            self.logger.info(os.getcwd())
+        #os.system("git remote add origin https://github.com/"+username+"/"+reponame+".git")
+        #os.system("git push -u origin master")
+        
+    #create a new repo for the projects
+    def create_new_repo(self,*args):
+        #creating a config dict to create a new repo 
+        repo = {
+            "name":args[0],
+            "description":"New repo for "+ args[0], 
+            "homepage":"https://github.com",
+            "private":False,
+            "has_issues":True,
+            "has_projects":True,
+            "has_wiki":True
+        }
+        url = self.git_api+"/user/repos"
+        create = self.session.post(url, json = repo)
+        if create.status_code == 201:
+            self.logger.info("successfully created repo %s....", args[0])
+        else:
+            self.logger.warning("Repo already exists for %s....", args[0])
+        self.push_project_github("preeyashp",args[0],args[1])
+        self.logger.info(args[1]+ " checking.....")
+        self.logger.info("Project pushed to the repository for {name}...".format(name = args[0]))
+
+    
---
+++ script.py
@@ -0,0 +1,55 @@
+import logging 
+import logging.config
+import os
+import json
+import argparse
+import subprocess
+from pathlib import Path
+import requestHandler
+
+
+logger = logging.getLogger(__name__)
+req = requestHandler.requestHandler(api_token = "69a84d0c58f832299f911644d7cc2f3386884075")
+
+#setting up the logging system 
+def setupLogging(default_path = "logging.json", default_level = logging.INFO):
+    if os.path.exists(default_path):
+        with open(default_path, 'rb') as file:
+            config = json.load(file)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level = default_level)
+
+def initializeGit(path):
+    subprocess.run(["git", "init", path])
+
+
+#creating a command line program
+def run(args):
+    dirPath = args.dir_path
+    req.connect()
+    if os.path.exists(dirPath):
+        logger.info("path exists...")
+        logger.info("Checking gits repos for {dir}.....".format(dir = os.path.abspath(dirPath)))
+        for dirs in Path(dirPath).iterdir():
+            if not any(folder.startswith('.git') for folder in os.listdir(dirs)):
+                initializeGit(dirs)
+                logger.info("git initialised")
+                
+            else:
+                logger.info("git already initialised for %s", dirs)
+            req.create_new_repo(os.path.basename(dirs),os.path.abspath(dirs))
+    else:
+        logger.error("Not found")
+
+def main():
+    setupLogging()
+    logger.info("Testing..")
+    parser = argparse.ArgumentParser("Initialize the git repo and upload it to the ")
+    parser.add_argument("-p", help = "folder path to initialize git", dest = "dir_path", type = str, required=True)
+    parser.set_defaults(func = run)
+    args = parser.parse_args()
+    args.func(args)
+
+if __name__ == "__main__":
+        main()
